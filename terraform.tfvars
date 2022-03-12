1f16a2d52c1c8696e9d67184a9effc61daa91b39
Josi-Edang <edangmacjosi@gmail.com>
2022-03-12 06:22:02 -0500

    updated resources

---
+++ terraform.tfvars
@@ -0,0 +1,8 @@
+
+vpc_cidr = var.vpc_cidr
+
+subnet_cidr = var.subnet_cidr
+
+instance_type = var.instance_type
+
+ami_id = var.ami_id
--- variable.tf
+++ variable.tf
@@ -4,8 +4,20 @@ variable "vpc_cidr" {
     description = "choose cidr_block"
   
 }
-variable "subnet_cidr" {
+variable "subnet_cidr"{
     type = string
     default = "10.0.1.0/24"
+    description = "choose subnet cidr"
+    }
+
+variable "instance_type"{
+    type = string
+    default = "t2.micro"
+    description = "must use cheap instance"
+}
+variable "ami_id" {
+    type = string
+    default = "ami-0359b3157f016ae46"
   
 }
+  
