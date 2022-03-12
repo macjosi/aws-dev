6ee4112400e81c2bd439016af15d25783447c495
Josi-Edang <edangmacjosi@gmail.com>
2022-03-12 05:59:04 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,47 @@
+resource "aws_vpc" "Foodvpc" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Mainvpc"
+  }
+}
+resource "aws_subnet" "FoodsubnetPb" {
+  vpc_id     = aws_vpc.Foodvpc.id
+  cidr_block = var.subnet_cidr
+
+  tags = {
+    Name = "MainPub"
+  }
+}
+resource "aws_subnet" "FoodsubnetPr" {
+  vpc_id     = aws_vpc.Foodvpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "MainPriv"
+  }
+}
+resource "aws_internet_gateway" "FoodIGW" {
+  vpc_id = aws_vpc.Foodvpc.id
+
+  tags = {
+    Name = "mainIGW"
+  }
+}
+resource "aws_route_table" "Foodrt" {
+  vpc_id = aws_vpc.Foodvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.FoodIGW.id
+  }
+}
+resource "aws_route_table_association" "subnetasso" {
+  subnet_id      = aws_subnet.FoodsubnetPb.id
+  route_table_id = aws_route_table.Foodrt.id
+}
+resource "aws_route_table_association" "igasso" {
+  gateway_id     = aws_internet_gateway.FoodIGW.id
+  route_table_id = aws_route_table.Foodrt.id
+}
---
+++ provider.tf
@@ -0,0 +1,6 @@
+
+provider "aws" {
+  region  = "us-west-2"
+  access_key = "AKIASMKM5FQHNDQL7O7Z"
+  secret_key = "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"
+}
